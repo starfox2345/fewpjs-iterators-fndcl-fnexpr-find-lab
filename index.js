de64544d43d8750cff837ef5acc0b6d27a369192
yatsun <yatsunlam25@gmail.com>
2021-09-12 23:26:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 
+superbowlWin = (record) => {
+    let result = record.find(record => record.result === 'W')
+    return !!result ? result.year : undefined
+    // Yeah alright, so it has to be 'result' no other
+}
